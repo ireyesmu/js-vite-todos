79f3153ec710f391557bb4b6d2accda557fc7bb0
Ignacio Reyes <ignacio.reyesmu@gmail.com>
2024-02-20 22:00:09 -0300

    Funciones declaradas en todo.store.js

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -2,9 +2,9 @@ import { Todo } from "../todos/models/todo.model";
 
 
 const Filters = {
-    All: 'all',
-    Completed: 'Completed',
-    Pending: 'Pending',
+    all: 'all',
+    completed: 'completed',
+    pending: 'pending',
 }
 
 
@@ -17,11 +17,85 @@ const state = {
     filter: Filters.All,
 }
 
+
 const initStore = () => {
     console.log(state);
     console.log('InitStore 🥑');
 }
 
+
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} filter 
+ */
+const getTodos = ( filter = Filters.all ) => {
+    switch( filter ) {
+        case Filters.all:
+            return [...state.todos];
+        case Filters.completed:
+            return state.todos.filter( todo => todo.done );
+        case Filters.pending:
+            return state.todos.filter( todo => !todo.done );
+        default:
+            throw new Error(`Option ${ filter } is not valid.`);
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    if ( !description ) throw new Error('Description is required');
+    state.todos.push( new Todo(description));
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done );
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.all ) => {
+    if ( !Object.keys(Filters).includes(newFilter) ) throw new Error(`Filtro ${newFilter} is not valid`);
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+
 export default {
+    addTodo,    
+    deleteCompleted,    
+    deleteTodo, 
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,  
+    toggleTodo, 
 }
